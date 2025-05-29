a50c1aab466009b642df54841e248c7ed80f7e19
Zheng-Changfu <zcfpromise@163.com>
2025-05-29 23:10:06 +0800

    feat: 增加 useLayoutMenu

--- packages/components/src/layout/types.ts
+++ packages/components/src/layout/types.ts
@@ -35,3 +35,23 @@ export interface LayoutTabbar {
    */
   height: number
 }
+
+/**
+ * 布局模式
+ * vertical: 竖向布局
+ * horizontal: 横向布局
+ * sidebar: 侧边栏布局
+ * mixed-sidebar: 混合侧边栏布局
+ * full-content: 全内容布局
+ * two-column: 双栏布局
+ * mixed-two-column: 混合双栏布局
+ */
+export type LayoutMode =
+  | 'vertical'
+  | 'horizontal'
+  | 'sidebar'
+  | 'mixed-sidebar'
+  | 'full-content'
+  | 'two-column'
+  | 'mixed-two-column'
+  | ({} & string)
