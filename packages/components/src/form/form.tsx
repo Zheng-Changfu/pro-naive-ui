4600501fd9d062fe327825924784145ca83084cf
Zheng-Changfu <zcfpromise@163.com>
2025-06-25 16:22:00 +0800

    fix(form): 修复表单使用回车无法提交问题

--- packages/components/src/form/form.tsx
+++ packages/components/src/form/form.tsx
@@ -2,7 +2,6 @@ import type { FormInst, FormProps } from 'naive-ui'
 import type { SlotsType } from 'vue'
 import type { ProFormProps } from './props'
 import type { ProFormSlots } from './slots'
-import { useEventListener } from '@vueuse/core'
 import { NForm } from 'naive-ui'
 import { provideInternalForm } from 'pro-composables'
 import { computed, defineComponent, onMounted, provide, ref } from 'vue'
@@ -56,7 +55,6 @@ export default defineComponent({
           e.preventDefault()
           if (!loading.value) {
             form.submit()
-            props.onSubmit && props.onSubmit(e)
           }
         },
         /**
@@ -66,6 +64,21 @@ export default defineComponent({
           e.preventDefault()
           form.restoreFieldsValue()
         },
+        /**
+         * 支持按下回车提交表单
+         * 因为 form 元素默认行为是支持按下回车提交的，所以如果没设置按下回车提交，这里要做阻止操作
+         */
+        onKeypress: (e: KeyboardEvent) => {
+          if (e.key !== 'Enter')
+            return
+          if (!overridedProps.value.submitOnPressEnter) {
+            e.preventDefault()
+            return
+          }
+          if (!loading.value) {
+            form.submit()
+          }
+        },
       }
     })
 
@@ -74,19 +87,6 @@ export default defineComponent({
       validationResults,
       registerProFormInst,
     } = form[proFormInternalKey]
-
-    /**
-     * form 元素默认行为是支持按下回车提交的，所以这里只需要做阻止操作即可
-     */
-    useEventListener(() => {
-      return overridedProps.value.submitOnPressEnter
-        ? null
-        : (nFormInst.value as any)?.$el
-    }, 'keypress', (e) => {
-      if (e.key === 'Enter') {
-        e.preventDefault()
-      }
-    })
 
     onMounted(() => {
       registerProFormInst({
