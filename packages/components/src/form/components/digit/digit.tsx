b6d7383648703c17886593840f89c36b4d8fcca1
Zheng-Changfu <zcfpromise@163.com>
2025-08-08 17:15:26 +0800

    refactor: 重构 pro-digit 组件

--- packages/components/src/form/components/digit/digit.tsx
+++ packages/components/src/form/components/digit/digit.tsx
@@ -1,53 +1,92 @@
-import type { SlotsType } from 'vue'
-import type { ProDigitProps } from './props'
+import type { InputNumberProps } from 'naive-ui'
+import type { SlotsType, VNodeChild } from 'vue'
 import type { ProDigitSlots } from './slots'
-import { defineComponent } from 'vue'
-import { useOverrideProps } from '../../../composables'
-import { ProField } from '../field'
-import { useMergePlaceholder } from '../field/composables/use-merge-placeholder'
-import Digit from './components/digit'
+import { NFlex, NInputNumber } from 'naive-ui'
+import { computed, defineComponent } from 'vue'
+import { useForwardRef } from '../../../composables/use-forward-ref'
+import { useFieldUtils, useProField } from '../field'
+import { ProFormItem } from '../form-item'
 import { provideDigitInstStore } from './inst'
 import { proDigitProps } from './props'
 
 const name = 'ProDigit'
 export default defineComponent({
   name,
+  inheritAttrs: false,
   props: proDigitProps,
   slots: Object as SlotsType<ProDigitSlots>,
-  setup(props, { expose }) {
+  setup(props) {
+    const forwardRef = useForwardRef()
+
     const {
-      exposed,
-    } = provideDigitInstStore()
+      field,
+      mergedReadonly,
+      proFormItemProps,
+      mergedFieldProps,
+    } = useProField<InputNumberProps>(props, name)
 
-    const placeholder = useMergePlaceholder(
-      name,
-      props,
-    )
+    const {
+      empty,
+      emptyDom,
+    } = useFieldUtils(field)
 
-    const overridedProps = useOverrideProps<ProDigitProps>(
-      name,
-      props,
-    )
-
-    expose(exposed)
+    const nInputNumberProps = computed(() => {
+      return {
+        ...mergedFieldProps.value,
+        value: field.value.value ?? null,
+      }
+    })
     return {
-      placeholder,
-      overridedProps,
+      field,
+      empty,
+      emptyDom,
+      forwardRef,
+      mergedReadonly,
+      proFormItemProps,
+      nInputNumberProps,
     }
   },
   render() {
+    if (!this.field.show.value) {
+      return
+    }
     return (
-      <ProField
-        {...this.overridedProps}
-        placeholder={this.placeholder}
-      >
+      <ProFormItem {...this.proFormItemProps}>
         {{
           ...this.$slots,
-          input: ({ inputProps }: any) => {
-            return <Digit {...inputProps} v-slots={this.$slots}></Digit>
+          default: () => {
+            let dom: VNodeChild
+            if (this.mergedReadonly) {
+              dom = this.empty
+                ? this.emptyDom
+                : (
+                    <NFlex size="small">
+                      {this.$slots.prefix && <span>{this.$slots.prefix()}</span>}
+                      <span>{this.field.value.value}</span>
+                      {this.$slots.suffix && <span>{this.$slots.suffix()}</span>}
+                    </NFlex>
+                  )
+            }
+            else {
+              dom = (
+                <NInputNumber
+                  ref={this.forwardRef}
+                  {...this.nInputNumberProps}
+                  v-slots={this.$slots}
+                >
+                </NInputNumber>
+              )
+            }
+            return this.$slots.input
+              ? this.$slots.input({
+                  inputDom: dom,
+                  readonly: this.mergedReadonly,
+                  inputProps: this.nInputNumberProps,
+                })
+              : dom
           },
         }}
-      </ProField>
+      </ProFormItem>
     )
   },
 })
