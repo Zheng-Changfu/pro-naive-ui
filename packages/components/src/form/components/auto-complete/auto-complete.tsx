fa08e96e17bf6cb85c0d79cf0ebfc666e531659e
Zheng-Changfu <zcfpromise@163.com>
2025-08-08 16:58:37 +0800

    refactor: 重构 pro-auto-complete 组件

--- packages/components/src/form/components/auto-complete/auto-complete.tsx
+++ packages/components/src/form/components/auto-complete/auto-complete.tsx
@@ -1,53 +1,91 @@
-import type { SlotsType } from 'vue'
-import type { ProAutoCompleteProps } from './props'
+import type { SlotsType, VNodeChild } from 'vue'
 import type { ProAutoCompleteSlots } from './slots'
-import { defineComponent } from 'vue'
-import { useOverrideProps } from '../../../composables'
-import { ProField } from '../field'
-import { useMergePlaceholder } from '../field/composables/use-merge-placeholder'
-import AutoComplete from './components/auto-complete'
-import { provideAutoCompleteInstStore } from './inst'
+import { NAutoComplete, NFlex } from 'naive-ui'
+import { computed, defineComponent } from 'vue'
+import { useForwardRef } from '../../../composables/use-forward-ref'
+import { useFieldUtils, useProField } from '../field'
+import { ProFormItem } from '../form-item'
 import { proAutoCompleteProps } from './props'
 
 const name = 'ProAutoComplete'
 export default defineComponent({
   name,
+  inheritAttrs: false,
   props: proAutoCompleteProps,
   slots: Object as SlotsType<ProAutoCompleteSlots>,
-  setup(props, { expose }) {
+  setup(props) {
+    const forwardRef = useForwardRef()
+
     const {
-      exposed,
-    } = provideAutoCompleteInstStore()
+      field,
+      mergedReadonly,
+      proFormItemProps,
+      mergedFieldProps,
+    } = useProField(props, name)
 
-    const placeholder = useMergePlaceholder(
-      name,
-      props,
-    )
+    const {
+      empty,
+      emptyDom,
+    } = useFieldUtils(field)
 
-    const overridedProps = useOverrideProps<ProAutoCompleteProps>(
-      name,
-      props,
-    )
+    const nAutoCompleteProps = computed(() => {
+      return {
+        ...mergedFieldProps.value,
+        value: field.value.value ?? '',
+      }
+    })
 
-    expose(exposed)
     return {
-      placeholder,
-      overridedProps,
+      field,
+      empty,
+      emptyDom,
+      forwardRef,
+      mergedReadonly,
+      proFormItemProps,
+      nAutoCompleteProps,
     }
   },
   render() {
+    if (!this.field.show.value) {
+      return
+    }
     return (
-      <ProField
-        {...this.overridedProps}
-        placeholder={this.placeholder}
-      >
+      <ProFormItem {...this.proFormItemProps}>
         {{
           ...this.$slots,
-          input: ({ inputProps }: any) => {
-            return <AutoComplete {...inputProps} v-slots={this.$slots}></AutoComplete>
+          default: () => {
+            let dom: VNodeChild
+            if (this.mergedReadonly) {
+              dom = this.empty
+                ? this.emptyDom
+                : (
+                    <NFlex size="small">
+                      {this.$slots.prefix && <span>{this.$slots.prefix()}</span>}
+                      <span>{this.field.value.value}</span>
+                      {this.$slots.suffix && <span>{this.$slots.suffix()}</span>}
+                    </NFlex>
+                  )
+            }
+            else {
+              dom = (
+                <NAutoComplete
+                  ref={this.forwardRef}
+                  {...this.nAutoCompleteProps}
+                  v-slots={this.$slots}
+                >
+                </NAutoComplete>
+              )
+            }
+            return this.$slots.input
+              ? this.$slots.input({
+                  inputDom: dom,
+                  readonly: this.mergedReadonly,
+                  inputProps: this.nAutoCompleteProps,
+                })
+              : dom
           },
         }}
-      </ProField>
+      </ProFormItem>
     )
   },
 })
--- packages/components/src/form/components/auto-complete/inst.ts
+++ packages/components/src/form/components/auto-complete/inst.ts
@@ -1,15 +1,3 @@
 import type { AutoCompleteInst } from 'naive-ui'
-import { createInjectionState } from '@vueuse/core'
-import { useComponentInst } from '../../../composables'
 
 export type ProAutoCompleteInst = AutoCompleteInst
-
-const [
-  provideAutoCompleteInstStore,
-  useInjectAutoCompleteInstStore,
-] = createInjectionState(useComponentInst<ProAutoCompleteInst>)
-
-export {
-  provideAutoCompleteInstStore,
-  useInjectAutoCompleteInstStore,
-}
