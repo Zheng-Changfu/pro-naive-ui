675e30e35dd5ae1f4744c50dfb33c32c0997d2e2
Zheng-Changfu <zcfpromise@163.com>
2025-08-08 17:24:36 +0800

    refactor: 重构 pro-input-otp 组件

--- packages/components/src/form/components/input-otp/input-otp.tsx
+++ packages/components/src/form/components/input-otp/input-otp.tsx
@@ -1,39 +1,99 @@
-import type { SlotsType } from 'vue'
-import type { ProInputOtpProps } from './props'
+import type { InputOtpProps } from 'naive-ui'
+import type { SlotsType, VNodeChild } from 'vue'
 import type { ProInputOtpSlots } from './slots'
-import { defineComponent } from 'vue'
-import { useOverrideProps } from '../../../composables'
-import { ProField } from '../field'
-import InputOtp from './components/input-otp'
+import { NFlex, NInputOtp } from 'naive-ui'
+import { computed, defineComponent } from 'vue'
+import { useFieldUtils, useProField } from '../field'
+import { ProFormItem } from '../form-item'
 import { proInputOtpProps } from './props'
 
 const name = 'ProInputOtp'
 export default defineComponent({
   name,
+  inheritAttrs: false,
   props: proInputOtpProps,
   slots: Object as SlotsType<ProInputOtpSlots>,
   setup(props) {
-    const overridedProps = useOverrideProps<ProInputOtpProps>(
-      name,
-      props,
-    )
+    const {
+      field,
+      mergedReadonly,
+      proFormItemProps,
+      mergedFieldProps,
+    } = useProField<InputOtpProps>(props, name)
+
+    const {
+      empty,
+      emptyDom,
+    } = useFieldUtils(field)
+
+    const nInputOtpProps = computed(() => {
+      return {
+        ...mergedFieldProps.value,
+        value: field.value.value ?? null,
+      }
+    })
 
     return {
-      overridedProps,
+      field,
+      empty,
+      emptyDom,
+      mergedReadonly,
+      proFormItemProps,
+      nInputOtpProps,
     }
   },
   render() {
+    if (!this.field.show.value) {
+      return
+    }
     return (
-      <ProField
-        {...this.overridedProps}
-      >
+      <ProFormItem {...this.proFormItemProps}>
         {{
           ...this.$slots,
-          input: ({ inputProps }: any) => {
-            return <InputOtp {...inputProps} v-slots={this.$slots}></InputOtp>
+          default: () => {
+            let dom: VNodeChild
+            if (this.mergedReadonly) {
+              if (this.$slots.default as any) {
+                dom = (
+                  <NInputOtp
+                    {...this.nInputOtpProps}
+                    readonly
+                    v-slots={this.$slots}
+                  >
+                  </NInputOtp>
+                )
+              }
+              else {
+                dom = this.empty
+                  ? this.emptyDom
+                  : this.nInputOtpProps.mask
+                    ? '********'
+                    : (
+                        <NFlex size="small">
+                          <span>{this.field.value.value}</span>
+                        </NFlex>
+                      )
+              }
+            }
+            else {
+              dom = (
+                <NInputOtp
+                  {...this.nInputOtpProps}
+                  v-slots={this.$slots}
+                >
+                </NInputOtp>
+              )
+            }
+            return this.$slots.input
+              ? this.$slots.input({
+                  inputDom: dom,
+                  readonly: this.mergedReadonly,
+                  inputProps: this.nInputOtpProps,
+                })
+              : dom
           },
         }}
-      </ProField>
+      </ProFormItem>
     )
   },
 })
