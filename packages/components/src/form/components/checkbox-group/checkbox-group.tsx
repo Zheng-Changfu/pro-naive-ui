9d70ed9df93dd7cabcee89e4d2ec5f284b76aa09
Zheng-Changfu <zcfpromise@163.com>
2025-08-08 16:59:47 +0800

    refactor: 重构 pro-checkbox-group 组件

--- packages/components/src/form/components/checkbox-group/checkbox-group.tsx
+++ packages/components/src/form/components/checkbox-group/checkbox-group.tsx
@@ -1,47 +1,142 @@
-import type { SlotsType } from 'vue'
-import type { ProCheckboxGroupProps } from './props'
+import type { CheckboxGroupProps } from 'naive-ui'
+import type { SlotsType, VNodeChild } from 'vue'
+import type { ProCheckboxGroupFieldProps } from './props'
 import type { ProCheckboxGroupSlots } from './slots'
-import { defineComponent } from 'vue'
-import { useOverrideProps } from '../../../composables'
-import { ProField } from '../field'
-import { useMergePlaceholder } from '../field/composables/use-merge-placeholder'
-import CheckboxGroup from './components/checkbox-group'
+import { get, toPath } from 'lodash-es'
+import { NCheckbox, NCheckboxGroup, NFlex } from 'naive-ui'
+import { computed, defineComponent } from 'vue'
+import { useFieldUtils, useProField } from '../field'
+import { ProFormItem } from '../form-item'
 import { proCheckboxGroupProps } from './props'
 
 const name = 'ProCheckboxGroup'
 export default defineComponent({
   name,
+  inheritAttrs: false,
   props: proCheckboxGroupProps,
   slots: Object as SlotsType<ProCheckboxGroupSlots>,
   setup(props) {
-    const placeholder = useMergePlaceholder(
-      name,
-      props,
-    )
+    const {
+      field,
+      mergedReadonly,
+      proFormItemProps,
+      mergedFieldProps,
+    } = useProField<ProCheckboxGroupFieldProps>(props, name)
 
-    const overridedProps = useOverrideProps<ProCheckboxGroupProps>(
-      name,
-      props,
-    )
+    const {
+      empty,
+      emptyDom,
+    } = useFieldUtils(field)
+
+    const normalizedOptions = computed(() => {
+      const {
+        options = [],
+        labelField = 'label',
+        valueField = 'value',
+      } = mergedFieldProps.value
+
+      const labelFieldPath = toPath(labelField)
+      const valueFieldPath = toPath(labelField)
+
+      return options.map((item) => {
+        const copyedItem = { ...item }
+        const label = get(item, labelField)
+        const value = get(item, valueField)
+        if (labelFieldPath.length > 0) {
+          delete copyedItem[labelFieldPath[0] as any]
+        }
+        if (valueFieldPath.length > 0) {
+          delete copyedItem[valueFieldPath[0] as any]
+        }
+        return {
+          ...copyedItem,
+          label,
+          value,
+        }
+      })
+    })
+
+    const nCheckboxGroupProps = computed<CheckboxGroupProps>(() => {
+      const {
+        options,
+        flexProps,
+        labelField,
+        valueField,
+        ...rest
+      } = mergedFieldProps.value
+      return {
+        ...rest,
+        value: field.value.value ?? null,
+      }
+    })
+
+    const selectedLabels = computed(() => {
+      const selectedValue = field.value.value ?? []
+      return normalizedOptions.value
+        .filter(item => selectedValue.includes(item.value))
+        .map(item => item.label)
+    })
 
     return {
-      placeholder,
-      overridedProps,
+      field,
+      empty,
+      emptyDom,
+      selectedLabels,
+      mergedReadonly,
+      proFormItemProps,
+      mergedFieldProps,
+      normalizedOptions,
+      nCheckboxGroupProps,
     }
   },
   render() {
+    if (!this.field.show.value) {
+      return
+    }
     return (
-      <ProField
-        {...this.overridedProps}
-        placeholder={this.placeholder}
-      >
+      <ProFormItem {...this.proFormItemProps}>
         {{
           ...this.$slots,
-          input: ({ inputProps }: any) => {
-            return <CheckboxGroup {...inputProps} v-slots={this.$slots}></CheckboxGroup>
+          default: () => {
+            let dom: VNodeChild
+            if (this.readonly) {
+              dom = this.empty
+                ? this.emptyDom
+                : <span>{this.selectedLabels.join('，')}</span>
+            }
+            else {
+              dom = (
+                <NCheckboxGroup {...this.nCheckboxGroupProps}>
+                  {{
+                    default: () => {
+                      const flexProps = this.mergedFieldProps.flexProps ?? {}
+                      return (
+                        <NFlex {...flexProps}>
+                          {
+                            this.normalizedOptions.map((item) => {
+                              return <NCheckbox key={item.value} {...item} />
+                            })
+                          }
+                        </NFlex>
+                      )
+                    },
+                  }}
+                </NCheckboxGroup>
+              )
+            }
+            return this.$slots.input
+              ? this.$slots.input({
+                  inputDom: dom,
+                  readonly: this.mergedReadonly,
+                  inputProps: {
+                    ...this.nCheckboxGroupProps,
+                    options: this.normalizedOptions,
+                  },
+                })
+              : dom
           },
         }}
-      </ProField>
+      </ProFormItem>
     )
   },
 })
