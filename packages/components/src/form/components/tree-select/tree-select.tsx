2e75f4ff7f474997e25a126dbd14f79df4b9fa23
Zheng-Changfu <zcfpromise@163.com>
2025-08-08 17:41:38 +0800

    refactor: 重构 pro-tree-select 组件

--- packages/components/src/form/components/tree-select/tree-select.tsx
+++ packages/components/src/form/components/tree-select/tree-select.tsx
@@ -1,53 +1,168 @@
-import type { SlotsType } from 'vue'
-import type { ProTreeSelectProps } from './props'
+import type { TreeSelectInst, TreeSelectOption, TreeSelectProps } from 'naive-ui'
+import type { SlotsType, VNodeChild } from 'vue'
 import type { ProTreeSelectSlots } from './slots'
-import { defineComponent } from 'vue'
-import { useOverrideProps } from '../../../composables'
-import { ProField } from '../field'
-import { useMergePlaceholder } from '../field/composables/use-merge-placeholder'
-import TreeSelect from './components/tree-select'
+import { get, isArray, noop } from 'lodash-es'
+import { NFlex, NTreeSelect } from 'naive-ui'
+import { eachTree } from 'pro-composables'
+import { computed, defineComponent } from 'vue'
+import { useForwardRef } from '../../../composables/use-forward-ref'
+import { useFieldUtils, useProField } from '../field'
+import { ProFormItem } from '../form-item'
 import { provideTreeSelectInstStore } from './inst'
 import { proTreeSelectProps } from './props'
+
+type Key = string | number
 
 const name = 'ProTreeSelect'
 export default defineComponent({
   name,
+  inheritAttrs: false,
   props: proTreeSelectProps,
   slots: Object as SlotsType<ProTreeSelectSlots>,
   setup(props, { expose }) {
+    const forwardRef = useForwardRef<TreeSelectInst>()
+
     const {
       exposed,
     } = provideTreeSelectInstStore()
 
-    const placeholder = useMergePlaceholder(
-      name,
-      props,
-    )
+    const {
+      field,
+      mergedReadonly,
+      proFormItemProps,
+      mergedFieldProps,
+    } = useProField<TreeSelectProps>(props, name)
 
-    const overridedProps = useOverrideProps<ProTreeSelectProps>(
-      name,
-      props,
-    )
+    const {
+      empty,
+      emptyDom,
+    } = useFieldUtils(field)
 
-    expose(exposed)
+    const nTreeSelectProps = computed(() => {
+      return {
+        ...mergedFieldProps.value,
+        value: field.value.value ?? null,
+      }
+    })
+
+    const optionKeyToInfoMap = computed(() => {
+      const {
+        options = [],
+        keyField = 'key',
+        childrenField = 'children',
+      } = mergedFieldProps.value
+
+      const map = new Map<Key, {
+        option: TreeSelectOption
+        parentValue: Key | null
+      }>()
+
+      eachTree(
+        options as TreeSelectOption[],
+        (item, _, { parent }) => {
+          const key = get(item, keyField) as Key
+          map.set(key, {
+            option: item,
+            parentValue: get(parent, keyField, null) as Key | null,
+          })
+        },
+        childrenField,
+      )
+      return map
+    })
+
+    const selectedLabels = computed(() => {
+      const labels: VNodeChild[] = []
+      const selectedValue = isArray(field.value.value) ? field.value.value : [field.value.value]
+      for (const value of selectedValue) {
+        const labelNodes = findLabelNodesByKey(value as Key)
+        labels.push(...labelNodes)
+      }
+      return labels
+    })
+
+    function findLabelNodesByKey(key: Key) {
+      const {
+        renderTag,
+        renderLabel,
+        showPath = false,
+        separator = ' / ',
+        labelField = 'label',
+      } = mergedFieldProps.value
+
+      const labels: VNodeChild[] = []
+      let info = optionKeyToInfoMap.value.get(key)
+      while (info) {
+        let label = get(info.option, labelField) as VNodeChild
+        if (renderTag) {
+          label = renderTag({ option: info.option, handleClose: noop })
+        }
+        if (renderLabel) {
+          label = renderLabel({ option: info.option, checked: true, selected: true })
+        }
+        if (label) {
+          if (labels.length) {
+            labels.unshift(separator)
+          }
+          labels.unshift(<span>{label}</span>)
+        }
+        if (!showPath) {
+          break
+        };
+        info = optionKeyToInfoMap.value.get(info.parentValue as any)
+      }
+      return labels
+    }
     return {
-      placeholder,
-      overridedProps,
+      field,
+      empty,
+      emptyDom,
+      forwardRef,
+      selectedLabels,
+      mergedReadonly,
+      proFormItemProps,
+      nTreeSelectProps,
     }
   },
   render() {
+    if (!this.field.show.value) {
+      return
+    }
     return (
-      <ProField
-        {...this.overridedProps}
-        placeholder={this.placeholder}
-      >
+      <ProFormItem {...this.proFormItemProps}>
         {{
           ...this.$slots,
-          input: ({ inputProps }: any) => {
-            return <TreeSelect {...inputProps} v-slots={this.$slots}></TreeSelect>
+          default: () => {
+            let dom: VNodeChild
+            if (this.mergedReadonly) {
+              dom = this.empty
+                ? this.emptyDom
+                : (
+                    <NFlex size="small">
+                      {this.selectedLabels}
+                    </NFlex>
+                  )
+            }
+            else {
+              dom = (
+                <NTreeSelect
+                  ref={this.forwardRef}
+                  {...this.nTreeSelectProps}
+                  v-slots={this.$slots}
+                >
+                </NTreeSelect>
+              )
+            }
+            return this.$slots.input
+              ? this.$slots.input({
+                  inputDom: dom,
+                  readonly: this.mergedReadonly,
+                  inputProps: this.nTreeSelectProps,
+                })
+              : dom
           },
         }}
-      </ProField>
+      </ProFormItem>
     )
   },
 })
