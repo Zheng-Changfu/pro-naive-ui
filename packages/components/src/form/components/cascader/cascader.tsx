4e660e24be3e0793b7ebc3fff8014d8d8b1fbe51
Zheng-Changfu <zcfpromise@163.com>
2025-08-08 16:59:02 +0800

    refactor: 重构 pro-cascader 组件

--- packages/components/src/form/components/cascader/cascader.tsx
+++ packages/components/src/form/components/cascader/cascader.tsx
@@ -1,53 +1,161 @@
-import type { SlotsType } from 'vue'
-import type { ProCascaderProps } from './props'
+import type { CascaderOption, CascaderProps } from 'naive-ui'
+import type { SlotsType, VNodeChild } from 'vue'
 import type { ProCascaderSlots } from './slots'
-import { defineComponent } from 'vue'
-import { useOverrideProps } from '../../../composables'
-import { ProField } from '../field'
-import { useMergePlaceholder } from '../field/composables/use-merge-placeholder'
-import Cascader from './components/cascader'
-import { provideCascaderInstStore } from './inst'
+import { get, isArray } from 'lodash-es'
+import { NCascader, NFlex } from 'naive-ui'
+import { eachTree } from 'pro-composables'
+import { computed, defineComponent } from 'vue'
+import { useForwardRef } from '../../../composables/use-forward-ref'
+import { useFieldUtils, useProField } from '../field'
+import { ProFormItem } from '../form-item'
 import { proCascaderProps } from './props'
+
+type ValueAtom = string | number
 
 const name = 'ProCascader'
 export default defineComponent({
   name,
+  inheritAttrs: false,
   props: proCascaderProps,
   slots: Object as SlotsType<ProCascaderSlots>,
-  setup(props, { expose }) {
+  setup(props) {
+    const forwardRef = useForwardRef()
+
     const {
-      exposed,
-    } = provideCascaderInstStore()
+      field,
+      mergedReadonly,
+      proFormItemProps,
+      mergedFieldProps,
+    } = useProField<CascaderProps>(props, name)
 
-    const placeholder = useMergePlaceholder(
-      name,
-      props,
-    )
+    const {
+      empty,
+      emptyDom,
+    } = useFieldUtils(field)
 
-    const overridedProps = useOverrideProps<ProCascaderProps>(
-      name,
-      props,
-    )
+    const nCascaderProps = computed(() => {
+      return {
+        ...mergedFieldProps.value,
+        value: field.value.value ?? null,
+      }
+    })
 
-    expose(exposed)
+    const optionValueToInfoMap = computed(() => {
+      const {
+        options = [],
+        valueField = 'value',
+        childrenField = 'children',
+      } = mergedFieldProps.value
+
+      const map = new Map<ValueAtom, {
+        option: CascaderOption
+        parentValue: ValueAtom | null
+      }>()
+
+      eachTree(
+        options as CascaderOption[],
+        (item, _, { parent }) => {
+          const value = get(item, valueField) as ValueAtom
+          map.set(value, {
+            option: item,
+            parentValue: get(parent, valueField, null) as ValueAtom | null,
+          })
+        },
+        childrenField,
+      )
+      return map
+    })
+
+    const selectedLabels = computed(() => {
+      const labels: VNodeChild[] = []
+      const selectedValue = isArray(field.value.value) ? field.value.value : [field.value.value]
+      for (const value of selectedValue) {
+        const labelNodes = findLabelNodesByValue(value as ValueAtom)
+        labels.push(...labelNodes)
+      }
+      return labels
+    })
+
+    function findLabelNodesByValue(value: ValueAtom) {
+      const {
+        renderLabel,
+        showPath = true,
+        separator = ' / ',
+        labelField = 'label',
+      } = mergedFieldProps.value
+
+      const labels: VNodeChild[] = []
+      let info = optionValueToInfoMap.value.get(value)
+      while (info) {
+        let label = get(info.option, labelField) as VNodeChild
+        if (renderLabel) {
+          label = renderLabel(info.option, true)
+        }
+        if (label) {
+          if (labels.length) {
+            labels.unshift(separator)
+          }
+          labels.unshift(<span>{label}</span>)
+        }
+        if (!showPath) {
+          break
+        };
+        info = optionValueToInfoMap.value.get(info.parentValue as any)
+      }
+      return labels
+    }
+
     return {
-      placeholder,
-      overridedProps,
+      field,
+      empty,
+      emptyDom,
+      forwardRef,
+      mergedReadonly,
+      nCascaderProps,
+      selectedLabels,
+      proFormItemProps,
+      mergedFieldProps,
     }
   },
   render() {
+    if (!this.field.show.value) {
+      return
+    }
     return (
-      <ProField
-        {...this.overridedProps}
-        placeholder={this.placeholder}
-      >
+      <ProFormItem {...this.proFormItemProps}>
         {{
           ...this.$slots,
-          input: ({ inputProps }: any) => {
-            return <Cascader {...inputProps} v-slots={this.$slots}></Cascader>
+          default: () => {
+            let dom: VNodeChild
+            if (this.readonly) {
+              dom = this.empty
+                ? this.emptyDom
+                : (
+                    <NFlex size="small">
+                      {this.selectedLabels}
+                    </NFlex>
+                  )
+            }
+            else {
+              dom = (
+                <NCascader
+                  ref={this.forwardRef}
+                  {...this.nCascaderProps}
+                  v-slots={this.$slots}
+                >
+                </NCascader>
+              )
+            }
+            return this.$slots.input
+              ? this.$slots.input({
+                  inputDom: dom,
+                  readonly: this.mergedReadonly,
+                  inputProps: this.nCascaderProps,
+                })
+              : dom
           },
         }}
-      </ProField>
+      </ProFormItem>
     )
   },
 })
--- packages/components/src/form/components/cascader/inst.ts
+++ packages/components/src/form/components/cascader/inst.ts
@@ -1,15 +1,3 @@
 import type { CascaderInst } from 'naive-ui'
-import { createInjectionState } from '@vueuse/core'
-import { useComponentInst } from '../../../composables'
 
 export type ProCascaderInst = CascaderInst
-
-const [
-  provideCascaderInstStore,
-  useInjectCascaderInstStore,
-] = createInjectionState(useComponentInst<ProCascaderInst>)
-
-export {
-  provideCascaderInstStore,
-  useInjectCascaderInstStore,
-}
