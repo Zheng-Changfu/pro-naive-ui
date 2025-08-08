65289e1a369ffc0789be8a4d641c6f40173916af
Zheng-Changfu <zcfpromise@163.com>
2025-08-08 17:35:02 +0800

    refactor: 重构 pro-slider 组件

--- packages/components/src/form/components/slider/slider.tsx
+++ packages/components/src/form/components/slider/slider.tsx
@@ -1,47 +1,77 @@
-import type { SlotsType } from 'vue'
-import type { ProSliderProps } from './props'
+import type { SliderProps } from 'naive-ui'
+import type { SlotsType, VNodeChild } from 'vue'
 import type { ProSliderSlots } from './slots'
-import { defineComponent } from 'vue'
-import { useOverrideProps } from '../../../composables'
-import { ProField } from '../field'
-import { useMergePlaceholder } from '../field/composables/use-merge-placeholder'
-import Slider from './components/slider'
+import { NSlider } from 'naive-ui'
+import { computed, defineComponent } from 'vue'
+import { useFieldUtils, useProField } from '../field'
+import { ProFormItem } from '../form-item'
 import { proSliderProps } from './props'
 
 const name = 'ProSlider'
 export default defineComponent({
   name,
+  inheritAttrs: false,
   props: proSliderProps,
   slots: Object as SlotsType<ProSliderSlots>,
   setup(props) {
-    const placeholder = useMergePlaceholder(
-      name,
-      props,
-    )
+    const {
+      field,
+      mergedReadonly,
+      proFormItemProps,
+      mergedFieldProps,
+    } = useProField<SliderProps>(props, name)
 
-    const overridedProps = useOverrideProps<ProSliderProps>(
-      name,
-      props,
-    )
+    const {
+      readonlyText,
+    } = useFieldUtils(field)
+
+    const nSliderProps = computed(() => {
+      return {
+        ...mergedFieldProps.value,
+        value: field.value.value ?? null as any,
+      }
+    })
 
     return {
-      placeholder,
-      overridedProps,
+      field,
+      readonlyText,
+      nSliderProps,
+      mergedReadonly,
+      proFormItemProps,
     }
   },
   render() {
+    if (!this.field.show.value) {
+      return
+    }
     return (
-      <ProField
-        {...this.overridedProps}
-        placeholder={this.placeholder}
-      >
+      <ProFormItem {...this.proFormItemProps}>
         {{
           ...this.$slots,
-          input: ({ inputProps }: any) => {
-            return <Slider {...inputProps} v-slots={this.$slots}></Slider>
+          default: () => {
+            let dom: VNodeChild
+            if (this.mergedReadonly) {
+              dom = this.readonlyText
+            }
+            else {
+              dom = (
+                <NSlider
+                  {...this.nSliderProps}
+                  v-slots={this.$slots}
+                >
+                </NSlider>
+              )
+            }
+            return this.$slots.input
+              ? this.$slots.input({
+                  inputDom: dom,
+                  readonly: this.mergedReadonly,
+                  inputProps: this.nSliderProps,
+                })
+              : dom
           },
         }}
-      </ProField>
+      </ProFormItem>
     )
   },
 })
