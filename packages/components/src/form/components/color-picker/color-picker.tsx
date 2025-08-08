103b877ad48fcdea583d0f2a8b11014aa15a0583
Zheng-Changfu <zcfpromise@163.com>
2025-08-08 17:00:00 +0800

    refactor: 重构 pro-color-picker 组件

--- packages/components/src/form/components/color-picker/color-picker.tsx
+++ packages/components/src/form/components/color-picker/color-picker.tsx
@@ -1,47 +1,84 @@
-import type { SlotsType } from 'vue'
-import type { ProColorPickerProps } from './props'
+import type { ColorPickerProps } from 'naive-ui'
+import type { SlotsType, VNodeChild } from 'vue'
 import type { ProColorPickerSlots } from './slots'
-import { defineComponent } from 'vue'
-import { useOverrideProps } from '../../../composables'
-import { ProField } from '../field'
-import { useMergePlaceholder } from '../field/composables/use-merge-placeholder'
-import ColorPicker from './components/color-picker'
+import { NColorPicker } from 'naive-ui'
+import { computed, defineComponent } from 'vue'
+import { useFieldUtils, useProField } from '../field'
+import { ProFormItem } from '../form-item'
 import { proColorPickerProps } from './props'
 
 const name = 'ProColorPicker'
 export default defineComponent({
   name,
+  inheritAttrs: false,
   props: proColorPickerProps,
   slots: Object as SlotsType<ProColorPickerSlots>,
   setup(props) {
-    const placeholder = useMergePlaceholder(
-      name,
-      props,
-    )
+    const {
+      field,
+      mergedReadonly,
+      proFormItemProps,
+      mergedFieldProps,
+    } = useProField<ColorPickerProps>(props, name)
 
-    const overridedProps = useOverrideProps<ProColorPickerProps>(
-      name,
-      props,
-    )
+    const {
+      empty,
+      emptyDom,
+    } = useFieldUtils(field)
+
+    const nColorPickerProps = computed(() => {
+      return {
+        ...mergedFieldProps.value,
+        value: field.value.value ?? null,
+        disabled: mergedReadonly.value || mergedFieldProps.value.disabled,
+      }
+    })
 
     return {
-      placeholder,
-      overridedProps,
+      field,
+      empty,
+      emptyDom,
+      mergedReadonly,
+      proFormItemProps,
+      nColorPickerProps,
     }
   },
   render() {
+    if (!this.field.show.value) {
+      return
+    }
     return (
-      <ProField
-        {...this.overridedProps}
-        placeholder={this.placeholder}
-      >
+      <ProFormItem {...this.proFormItemProps}>
         {{
           ...this.$slots,
-          input: ({ inputProps }: any) => {
-            return <ColorPicker {...inputProps} v-slots={this.$slots}></ColorPicker>
+          default: () => {
+            const slots = {
+              ...this.$slots,
+              label: this.$slots['picker-label'],
+            }
+            let dom: VNodeChild
+            if (this.mergedReadonly && this.empty) {
+              dom = this.emptyDom
+            }
+            else {
+              dom = (
+                <NColorPicker
+                  {...this.nColorPickerProps}
+                  v-slots={slots}
+                >
+                </NColorPicker>
+              )
+            }
+            return this.$slots.input
+              ? this.$slots.input({
+                  inputDom: dom,
+                  readonly: this.mergedReadonly,
+                  inputProps: this.nColorPickerProps,
+                })
+              : dom
           },
         }}
-      </ProField>
+      </ProFormItem>
     )
   },
 })
