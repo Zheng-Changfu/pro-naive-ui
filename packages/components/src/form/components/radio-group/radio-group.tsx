8683e266e9948f6ae34d0f561f9c3dea64f87ef1
Zheng-Changfu <zcfpromise@163.com>
2025-08-08 17:28:55 +0800

    refactor: 重构 pro-radio-group 组件

--- packages/components/src/form/components/radio-group/radio-group.tsx
+++ packages/components/src/form/components/radio-group/radio-group.tsx
@@ -1,47 +1,147 @@
-import type { SlotsType } from 'vue'
-import type { ProRadioGroupProps } from './props'
+import type { FlexProps, RadioGroupProps, RadioProps } from 'naive-ui'
+import type { PropType, SlotsType, VNodeChild } from 'vue'
+import type { ProRadioGroupFieldProps } from './props'
 import type { ProRadioGroupSlots } from './slots'
-import { defineComponent } from 'vue'
-import { useOverrideProps } from '../../../composables'
-import { ProField } from '../field'
-import { useMergePlaceholder } from '../field/composables/use-merge-placeholder'
-import RadioGroup from './components/radio-group'
+import { get, toPath } from 'lodash-es'
+import { NFlex, NRadio, NRadioButton, NRadioGroup } from 'naive-ui'
+import { computed, defineComponent } from 'vue'
+import { useFieldUtils, useProField } from '../field'
+import { ProFormItem } from '../form-item'
 import { proRadioGroupProps } from './props'
 
 const name = 'ProRadioGroup'
 export default defineComponent({
   name,
+  inheritAttrs: false,
   props: proRadioGroupProps,
   slots: Object as SlotsType<ProRadioGroupSlots>,
   setup(props) {
-    const placeholder = useMergePlaceholder(
-      name,
-      props,
-    )
+    const {
+      field,
+      mergedReadonly,
+      proFormItemProps,
+      mergedFieldProps,
+    } = useProField<ProRadioGroupFieldProps>(props, name)
 
-    const overridedProps = useOverrideProps<ProRadioGroupProps>(
-      name,
-      props,
-    )
+    const {
+      empty,
+      emptyDom,
+    } = useFieldUtils(field)
+
+    const normalizedOptions = computed(() => {
+      const {
+        options = [],
+        labelField = 'label',
+        valueField = 'value',
+      } = mergedFieldProps.value
+
+      const labelFieldPath = toPath(labelField)
+      const valueFieldPath = toPath(valueField)
+
+      return options.map((item) => {
+        const copyedItem = { ...item }
+        const label = get(item, labelField)
+        const value = get(item, valueField)
+        if (labelFieldPath.length > 0) {
+          delete copyedItem[labelFieldPath[0] as any]
+        }
+        if (valueFieldPath.length > 0) {
+          delete copyedItem[valueFieldPath[0] as any]
+        }
+        return {
+          ...copyedItem,
+          label,
+          value,
+        }
+      })
+    })
+
+    const nRadioGroupProps = computed<RadioGroupProps>(() => {
+      const {
+        type,
+        options,
+        flexProps,
+        labelField,
+        valueField,
+        ...rest
+      } = mergedFieldProps.value
+      return {
+        ...rest,
+        value: field.value.value ?? null,
+      }
+    })
+
+    const selectedLabel = computed(() => {
+      const selectedValue = field.value.value
+      const valueLabelOption = normalizedOptions.value.find(item => item.value === selectedValue)
+      return valueLabelOption ? (valueLabelOption.label ?? selectedValue) : selectedValue
+    })
 
     return {
-      placeholder,
-      overridedProps,
+      field,
+      empty,
+      emptyDom,
+      selectedLabel,
+      mergedReadonly,
+      proFormItemProps,
+      mergedFieldProps,
+      nRadioGroupProps,
+      normalizedOptions,
     }
   },
   render() {
+    if (!this.field.show.value) {
+      return
+    }
     return (
-      <ProField
-        {...this.overridedProps}
-        placeholder={this.placeholder}
-      >
+      <ProFormItem {...this.proFormItemProps}>
         {{
           ...this.$slots,
-          input: ({ inputProps }: any) => {
-            return <RadioGroup {...inputProps} v-slots={this.$slots}></RadioGroup>
+          default: () => {
+            let dom: VNodeChild
+            if (this.mergedReadonly) {
+              dom = this.empty
+                ? this.emptyDom
+                : <span>{this.selectedLabel}</span>
+            }
+            else {
+              dom = (
+                <NRadioGroup {...this.nRadioGroupProps}>
+                  {{
+                    default: () => {
+                      const { type, flexProps = {} } = this.mergedFieldProps
+                      if (type === 'button') {
+                        return this.normalizedOptions.map((item) => {
+                          return <NRadioButton key={item.value} {...item} />
+                        })
+                      }
+                      return (
+                        <NFlex {...flexProps}>
+                          {
+                            this.normalizedOptions.map((item) => {
+                              return <NRadio key={item.value} {...item} />
+                            })
+                          }
+                        </NFlex>
+                      )
+                    },
+                  }}
+                </NRadioGroup>
+              )
+            }
+            return this.$slots.input
+              ? this.$slots.input({
+                  inputDom: dom,
+                  readonly: this.mergedReadonly,
+                  inputProps: {
+                    ...this.nRadioGroupProps,
+                    options: this.normalizedOptions,
+                  },
+                })
+              : dom
           },
         }}
-      </ProField>
+      </ProFormItem>
     )
   },
 })
