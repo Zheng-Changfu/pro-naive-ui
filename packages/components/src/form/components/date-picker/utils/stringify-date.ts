5b660c5da52b69a2908cd5df731a2d231b2012c0
Zheng-Changfu <zcfpromise@163.com>
2025-10-13 14:07:32 +0800

    fix: 修复 valueFormat 在只读模式下显示错误问题，closes https://github.com/Zheng-Changfu/pro-naive-ui/issues/104

--- packages/components/src/form/components/date-picker/utils/stringify-date.ts
+++ packages/components/src/form/components/date-picker/utils/stringify-date.ts
@@ -1,9 +1,24 @@
 import { format, isDate } from 'date-fns'
 import { isArray, isNumber, isString } from 'lodash-es'
 
+function isTimestampString(value: string) {
+  if (!/^\d+$/.test(value)) {
+    return false
+  }
+  const len = value.length
+  const secondsTimestampLen = 10
+  const msTimestampLen = 13
+  const usTimestampLen = 16
+  return [
+    secondsTimestampLen,
+    msTimestampLen,
+    usTimestampLen,
+  ].includes(len)
+}
+
 export function stringifyDate(value: any, pattern: string): string | string[] | null {
   if (isString(value)) {
-    if (/^\d+$/.test(value)) {
+    if (isTimestampString(value)) {
       return format(Number(value), pattern, {
         useAdditionalWeekYearTokens: true,
       })
