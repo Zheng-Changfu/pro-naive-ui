940abf2aaba2d6f1aa5d3c2a3617b25fa22301f4
Zheng-Changfu <zcfpromise@163.com>
2026-03-13 11:25:54 +0800

    fix: 修复 modal form 无法自适应高度问题

--- packages/components/src/modal-form/styles/index.cssr.ts
+++ packages/components/src/modal-form/styles/index.cssr.ts
@@ -2,7 +2,11 @@ import { c, cB, cE } from 'naive-ui'
 
 export default c([
   cB('card', [
+    // 兼容 naive-ui 2.44.0 以下
     cE('content', `
+      overflow: auto; 
+    `),
+    cB('card-content', `
       overflow: auto; 
     `),
   ]),
